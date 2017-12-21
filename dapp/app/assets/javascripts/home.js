574dcccc39c8d95372a2e8e7d8b1430ae76cb55e
arasmarko <marko@arsfutura.co>
2017-12-21 22:40:24 +0100

    Connected to my contract

--- dapp/app/assets/javascripts/home.js
+++ dapp/app/assets/javascripts/home.js
@@ -1,32 +1,51 @@
+var contractAddress = "0xF288661e101aC232139Db8C27A1072Aa5E4A275E";
+var contractAbi = [ { "constant": false, "inputs": [], "name": "kill", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "myNewVar", "type": "uint256" } ], "name": "setMyVariable", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "getMyContractBalance", "outputs": [ { "name": "", "type": "uint256", "value": "0" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "getMyVariable", "outputs": [ { "name": "", "type": "uint256", "value": "5" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "mortal", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "payable": true, "stateMutability": "payable", "type": "constructor" }, { "payable": true, "stateMutability": "payable", "type": "fallback" } ]
+var myContract = web3.eth.contract(contractAbi).at(contractAddress);
+
 $(document).on('page:ready page:change', function (){
 
 	console.log("message");
 
-	if (typeof web3 !== 'undefined') {
-		web3 = new Web3(web3.currentProvider);
-	} else {
-		// set the provider you want from Web3.providers
-		web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-	}
+	// if (typeof web3 !== 'undefined') {
+	// 	web3 = new Web3(web3.currentProvider);
+	// } else {
+	// 	// set the provider you want from Web3.providers
+	// 	web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+	// }
+
+	// Checking if Web3 has been injected by the browser (Mist/MetaMask)
+	  if (typeof web3 !== 'undefined') {
+	    // Use Mist/MetaMask's provider
+	    window.web3 = new Web3(web3.currentProvider);
+	  } else {
+	    console.log('No web3? You should consider trying MetaMask!')
+	    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+	    window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+	  }
+
+	
 
 })
 
-$(document).on('click', '#getBalance', function(e) {
-	getBalance()
+$(document).on('click', '#getCounter', function(e) {
+	console.log("getCounter");
+	myContract.getMyVariable(function(error, result){
+	if(!error)
+		console.log(result)
+	else
+		console.error(error);
+ 	});
+	// document.getElementById("myCounter").innerHtml = myContract.getMyVariable();
+
 });
 
-function getBalance() {
-	console.log("getBalance", web3.eth.accounts);
-	// document.getElementById("myBalance").innerHtml = web3.eth.getBalance(web3.eth.accounts[0]);
-	_getBalance(web3.eth.accounts[0]);
-}
 
-function _getBalance (address) {
-  return web3.eth.getBalance(address, function (error, result) {
-    if (!error) {
-      console.log(result.toNumber());
-    } else {
-      console.error(error);
-    }
-  })
-}
+// function _getBalance (address) {
+//   return web3.eth.getBalance(address, function (error, result) {
+//     if (!error) {
+//       console.log(result.toNumber());
+//     } else {
+//       console.error(error);
+//     }
+//   })
+// }
